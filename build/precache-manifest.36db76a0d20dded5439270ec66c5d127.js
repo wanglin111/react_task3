c1eeb1260e1573dab1c05f4bdd88acc1ba4934b9
wanglin111 <273215924@qq.com>
2020-10-25 11:47:25 +0800

    task3

---
+++ build/precache-manifest.36db76a0d20dded5439270ec66c5d127.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b0c7341c90ac64596b4929cd6cfe23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d08e0a54074b643ca147",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "e04e22a05c2d1c75fe1e",
+    "url": "/static/css/main.966ab29a.chunk.css"
+  },
+  {
+    "revision": "d08e0a54074b643ca147",
+    "url": "/static/js/2.bf8228ea.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.bf8228ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04e22a05c2d1c75fe1e",
+    "url": "/static/js/main.5aab3f85.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "63cad02f7c2f497a8a08f8d1112836cb",
+    "url": "/static/media/wan.63cad02f.png"
+  }
+]);
